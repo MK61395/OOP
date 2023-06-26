021bda73a857465cda2263143067ffa0905e2334
Muhammad Kashif <91327646+MK61395@users.noreply.github.com>
2023-06-26 19:33:15 +0500

    Add files via upload

---
+++ CSR.cpp
@@ -0,0 +1,48 @@
+//Muhammad Kashif
+//I21-0851
+//Section Y
+//Assignment#2
+
+//#include "pch.h"
+//#include "CSR.h"
+//
+//int CSR::getCSRID() {
+//	return csrID;
+//};
+//char* CSR::getName() {
+//	return csrName;
+//};
+//int CSR::getHours() {
+//	return hours;
+//};
+//int CSR::getComplaintsResolved() {
+//	return complaintsResolved;
+//};
+//float CSR::getPayrate() {
+//	return payrate;
+//};
+//float CSR::getWage() {
+//	return wage;
+//};
+//void CSR::setCSRID(int ID) {
+//	csrID = ID;
+//};
+//void CSR::setName(char* n) {
+//	csrName = n;
+//};
+//void CSR::setHours(int h) {
+//	hours = h;
+//};
+//void CSR::setComplaintsResolved(int cpsResolved) {
+//	complaintsResolved = cpsResolved;
+//};
+//void CSR::setTotalCpsResolved(int totalcps) {
+//	totalComplaintsResolved = totalcps;
+//};
+////void CSR::calcPayrate() {
+////	payrate = 25 + 25 * (complaintsResolved / totalComplaintsResolved);
+////};
+//void CSR::calcWage() {
+//	wage = hours * payrate;
+//};
+////static int getTotalCpsResolved();
